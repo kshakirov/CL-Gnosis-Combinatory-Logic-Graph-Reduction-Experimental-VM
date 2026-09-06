ff8c2727f3dbc7b60afe9059754e3073fdf3ddae
kshakirov <kirill.shakirov4@gmai.com>
2026-09-06 21:08:10 +0300

    issue 2 wip 1

--- src/main.c
+++ src/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <inttypes.h>
+#include <assert.h>
+enum {
+  ATOM,
+  APP
+};
+
+struct Node {
+  uint32_t tag;
+  uint32_t lindex;
+  uint32_t rindex;
+  uint32_t reserved;
+    
+};
 
 int main(void){
   printf("Approaching Curry\n");
+  static_assert(sizeof(struct Node) == 16);
   return 0;
 }
