52db42a1d5bfe9407168c78259aaa6b077ccb427
kshakirov <kirill.shakirov4@gmai.com>
2026-09-06 21:20:45 +0300

    issue 2 wip 2

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <inttypes.h>
 #include <assert.h>
 enum {
+  INVALID,
   ATOM,
   APP
 };
@@ -17,5 +18,12 @@ struct Node {
 int main(void){
   printf("Approaching Curry\n");
   static_assert(sizeof(struct Node) == 16);
+  struct Node arena[2];
+  struct Node x;
+  x.tag = ATOM;
+  arena[0]=x;
+  arena[1].tag=INVALID;
+  printf("The arena now contains one real node %d and one invalid %d \n", arena[0].tag, arena[1].tag);
+
   return 0;
 }
