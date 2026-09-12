52ef82598951604df055048ca6afdef17fd8a4b8
kshakirov <kirill.shakirov4@gmai.com>
2026-09-12 22:37:50 +0300

    Materialize an application node in the arena

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
+
 #include <stdio.h>
 #include <inttypes.h>
 #include <assert.h>
+#include <string.h>  // ✅ В C используется .h
 enum {
   INVALID,
   ATOM,
@@ -15,15 +17,27 @@ struct Node {
     
 };
 
+
+extern void curry_app(
+    struct Node *arena,
+    uint32_t destination,
+    uint32_t scale,
+    uint32_t left,
+    uint32_t right
+);
+
+
 int main(void){
   printf("Approaching Curry\n");
   static_assert(sizeof(struct Node) == 16);
-  struct Node arena[2];
-  struct Node x;
-  x.tag = ATOM;
-  arena[0]=x;
-  arena[1].tag=INVALID;
-  printf("The arena now contains one real node %d and one invalid %d \n", arena[0].tag, arena[1].tag);
-
+  struct Node arena[3]= {0};
+  arena[0].tag = ATOM;
+  arena[1].tag = ATOM;
+  
+  curry_app(arena,2,sizeof(struct Node),0,1);
+  //  assert(arena_1.tag == arena_2.tag);
+  //curry_app(&arena_1,1,0,0);
+  printf("The arena now contains  2 atoms and on ob applcation its tag is  %d the left index is  %d the right is %d \n", arena[2].tag, arena[2].lindex, arena[2].rindex);
+  
   return 0;
 }
